0be19cb80b6db661cd550a2c263b3b7dea9e2553
ypatel2022 <yaxpatel2004@gmail.com>
2023-05-20 16:29:36 -0400

    setup basic oled display logic

---
+++ oled-display/oled-display.ino
@@ -0,0 +1,116 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for SSD1306 display connected using I2C
+#define OLED_RESET -1 // Reset pin
+#define SCREEN_ADDRESS 0x3C
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+void setup()
+{
+  Serial.begin(9600);
+
+  // initialize the OLED object
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS))
+  {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;)
+      ; // Don't proceed, loop forever
+  }
+
+  // Clear the buffer.
+  display.clearDisplay();
+
+  // Display Text
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 28);
+  display.println("TESTING Hello world!");
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+
+  // Display Inverted Text
+  display.setTextColor(BLACK, WHITE); // 'inverted' text
+  display.setCursor(0, 28);
+  display.println("TESTING Hello world!");
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+
+  // Changing Font Size
+  display.setTextColor(WHITE);
+  display.setCursor(0, 24);
+  display.setTextSize(2);
+  display.println("Hello!");
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+
+  // Display Numbers
+  display.setTextSize(1);
+  display.setCursor(0, 28);
+  display.println(123456789);
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+
+  // Specifying Base For Numbers
+  display.setCursor(0, 28);
+  display.print("0x");
+  display.print(0xFF, HEX);
+  display.print("(HEX) = ");
+  display.print(0xFF, DEC);
+  display.println("(DEC)");
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+
+  // Display ASCII Characters
+  display.setCursor(0, 24);
+  display.setTextSize(2);
+  display.write(3);
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+
+  // Scroll full screen
+  display.setCursor(0, 0);
+  display.setTextSize(1);
+  display.println("Full");
+  display.println("screen");
+  display.println("scrolling!");
+  display.display();
+  display.startscrollright(0x00, 0x07);
+  delay(2000);
+  display.stopscroll();
+  delay(1000);
+  display.startscrollleft(0x00, 0x07);
+  delay(2000);
+  display.stopscroll();
+  delay(1000);
+  display.startscrolldiagright(0x00, 0x07);
+  delay(2000);
+  display.startscrolldiagleft(0x00, 0x07);
+  delay(2000);
+  display.stopscroll();
+  display.clearDisplay();
+
+  // Scroll part of the screen
+  display.setCursor(0, 0);
+  display.setTextSize(1);
+  display.println("Scroll");
+  display.println("some part");
+  display.println("of the screen.");
+  display.display();
+  display.startscrollright(0x00, 0x00);
+}
+
+void loop()
+{
+}
