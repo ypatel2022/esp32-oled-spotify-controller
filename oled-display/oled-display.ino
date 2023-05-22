15af293d842d03a28c396325a69f3d60253ac38e
ypatel2022 <yaxpatel2004@gmail.com>
2023-05-22 18:17:33 -0400

    reduce api call amount and change to new updated route

--- oled-display/oled-display.ino
+++ oled-display/oled-display.ino
@@ -20,9 +20,11 @@ unsigned long previousDisplayMillis = 0;
 
 const char *album = "";
 const char *artist = "";
-const char *song = "this is a test song name before it gets updated";
-long long timestamp = 0;
-long progress = 0;
+const char *song = "";
+const char *progress = "";
+const char *duration = "";
+const char *nextSong = "";
+const char *nextArtist = "";
 
 void setup()
 {
@@ -72,73 +74,63 @@ void loop()
     {
       HTTPClient client;
 
-      client.begin(SERVER_IP + String("/is-playing"));
+      client.begin(SERVER_IP + String("/current-playback-state"));
       int httpCode = client.GET();
 
       if (httpCode > 0)
       {
         String payload = client.getString();
-        // Serial.println("\nStatus Code: " + String(httpCode));
-        // Serial.println(payload);
 
-        if (payload == "true")
+        if (payload != "error")
         {
-          // Serial.println("Getting Currently Playing Song");
+          StaticJsonDocument<0> filter;
+          filter.set(true);
 
-          client.begin(SERVER_IP + String("/currently-playing"));
-          httpCode = client.GET();
+          StaticJsonDocument<384> doc;
 
-          if (httpCode > 0)
+          DeserializationError error = deserializeJson(doc, payload, DeserializationOption::Filter(filter));
+
+          if (error)
           {
-            payload = client.getString();
-            // Serial.println("\nStatus Code: " + String(httpCode));
-            // Serial.println(payload);
-
-            StaticJsonDocument<0> filter;
-            filter.set(true);
-
-            StaticJsonDocument<192> doc;
-
-            DeserializationError error = deserializeJson(doc, payload, DeserializationOption::Filter(filter));
-
-            if (error)
-            {
-              Serial.print("deserializeJson() failed: ");
-              Serial.println(error.c_str());
-              return;
-            }
-
-            album = doc["album"];         // "Luv Is Rage 2"
-            artist = doc["artist"];       // "Lil Uzi Vert"
-            song = doc["song"];           // "The Way Life Goes (feat. Oh Wonder)"
-            timestamp = doc["timestamp"]; // 1684637890615
-            progress = doc["progress"];   // 166610
-
-            // display the text on the screen
-            display.clearDisplay();
-            display.setTextColor(WHITE);
-
-            // display.setCursor(x, 0);
-            display.setCursor(0, 0);
-            display.setTextSize(1);
-            display.print(String(song));
-            // display.startscrollleft(0x00, 0x00);
-
-            display.setCursor(0, 10);
-            display.print(String(artist));
-
-            display.setCursor(0, 20);
-            display.print(String(album));
-
-            display.setCursor(0, 30);
-            display.print(String(miliseconds_to_minutes(progress)));
-
-            display.display();
+            Serial.print("deserializeJson() failed: ");
+            Serial.println(error.c_str());
+            return;
           }
-          else
-          {
-            Serial.println("Error on HTTP request");
-          }
+
+          album = doc["album"];
+          artist = doc["artists"];
+          song = doc["song"];
+          progress = doc["progress"];
+          duration = doc["duration"];
+          nextSong = doc["nextSong"];
+          nextArtist = doc["nextArtist"];
+
+          // display the text on the screen
+          display.clearDisplay();
+
+          // display.setCursor(x, 0);
+          display.setCursor(0, 0);
+          display.setTextSize(1);
+          display.print(String(song));
+          // display.startscrollleft(0x00, 0x00);
+
+          display.setCursor(0, 10);
+          display.print(String(artist));
+
+          display.setCursor(0, 20);
+          display.print(String(album));
+
+          display.setCursor(0, 30);
+          display.print(String(progress) + String(" / ") + String(duration));
+
+          // display next song
+          display.setCursor(0, 40);
+          display.print(String(">>> ") + String(nextSong));
+
+          display.setCursor(0, 50);
+          display.print(String("    ") + String(nextArtist));
+
+          display.display();
         }
 
         client.end();
@@ -153,19 +145,4 @@ void loop()
       Serial.println("Not connected to WiFi");
     }
   }
-}
-
-String miliseconds_to_minutes(long miliseconds)
-{
-  long seconds = miliseconds / 1000;
-  long minutes = seconds / 60;
-  seconds = seconds % 60;
-
-  String secondsString = String(seconds);
-  if (seconds < 10)
-  {
-    secondsString = "0" + secondsString;
-  }
-
-  return String(minutes) + ":" + secondsString;
 }
